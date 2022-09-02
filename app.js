221916840faddd02a9180cbc6c44520693787d8f
Shreya Wanisha <75380289+shreya2430@users.noreply.github.com>
2022-09-03 02:58:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { nanoid } from "nanoid";
+import React, { useState } from "react";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+function addTask(name) {
+  const newTask = { id: "id", name, completed: false };
+  setTasks([...tasks, newTask]);
+}
+function App(props) {
+const [tasks, setTasks] = useState(props.tasks);
+  const taskList = props.tasks.map((task) => (
+    <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+      />
+    )
+  );
+const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
